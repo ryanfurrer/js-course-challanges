a4b73928ebd095c42415fbabb0b46eada4b6f31e
Ryan Furrer <ryanfurrer@hey.com>
2023-08-07 19:55:58 -0400

    Completed project 06-modal.

--- javascript-basic-projects-master/06-modal/setup/app.js
+++ javascript-basic-projects-master/06-modal/setup/app.js
@@ -2,3 +2,14 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const openModalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeModalBtn = document.querySelector('.close-btn');
+
+openModalBtn.addEventListener('click', function () {
+	modalOverlay.classList.toggle('open-modal');
+});
+closeModalBtn.addEventListener('click', function () {
+	modalOverlay.classList.remove('open-modal');
+});
