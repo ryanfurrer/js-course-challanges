d6515ddb48042c0f968a08edf67f6a5296ea9254
Ryan Furrer <ryanfurrer@hey.com>
2023-08-08 19:42:36 -0400

    Completed project 07-questions.

--- javascript-basic-projects-master/07-questions/setup/app.js
+++ javascript-basic-projects-master/07-questions/setup/app.js
@@ -1,10 +1,28 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
+// Traversing DOM approach
 
-btns.forEach(function (btn) {
-	btn.addEventListener('click', function (e) {
-		e.currentTarget.parentElement.parentElement.classList.toggle('show-text');
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function (btn) {
+// 	btn.addEventListener('click', function (e) {
+// 		e.currentTarget.parentElement.parentElement.classList.toggle('show-text');
+// 	});
+// });
+
+// Using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+	btn = question.querySelector('.question-btn');
+	btn.addEventListener('click', function () {
+		questions.forEach(function (item) {
+			if (item !== question) {
+				item.classList.remove('show-text');
+			}
+		});
+		question.classList.toggle('show-text');
 	});
 });
