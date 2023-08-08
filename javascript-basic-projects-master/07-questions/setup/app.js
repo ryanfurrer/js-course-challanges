f4f5c990788e8d0d523494a44b5e508fd7c47226
Ryan Furrer <ryanfurrer@hey.com>
2023-08-07 21:16:08 -0400

    Completed project 07-questions.

--- javascript-basic-projects-master/07-questions/setup/app.js
+++ javascript-basic-projects-master/07-questions/setup/app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+	btn.addEventListener('click', function (e) {
+		e.currentTarget.parentElement.parentElement.classList.toggle('show-text');
+	});
+});
