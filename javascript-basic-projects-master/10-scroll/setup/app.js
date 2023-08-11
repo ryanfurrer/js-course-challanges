44cff3f4ce527d1fbb5972048c9fc4e751e8e3e2
Ryan Furrer <ryanfurrer@hey.com>
2023-08-11 17:40:12 -0400

    Completed through video 168. Scroll-Toggle Dynamic

--- javascript-basic-projects-master/10-scroll/setup/app.js
+++ javascript-basic-projects-master/10-scroll/setup/app.js
@@ -4,10 +4,45 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const footerDate = document.querySelector('#date');
+footerDate.innerText = new Date().getFullYear().toString();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+const containerHeight = linksContainer.getBoundingClientRect().height;
+const linksHeight = links.getBoundingClientRect().height;
+
+navToggle.addEventListener('click', function () {
+	if (containerHeight === 0) {
+		linksContainer.style.height = `${linksHeight}px`;
+	} else {
+		linksContainer.style.height = 0;
+	}
+});
 
 // ********** fixed navbar ************
+const navbar = document.querySelector('#nav');
+const navbarHeight = navbar.getBoundingClientRect().height;
+const header = document.querySelector('#home');
+const headerHeight = header.getBoundingClientRect().height;
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll', function () {
+	if (window.scrollY > navbarHeight) {
+		navbar.classList.add('fixed-nav');
+	} else {
+		navbar.classList.remove('fixed-nav');
+	}
+
+	if (window.scrollY > headerHeight) {
+		topLink.classList.add('show-link');
+	} else {
+		topLink.classList.remove('show-link');
+	}
+});
 
 // ********** smooth scroll ************
 // select links
